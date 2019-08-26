6b2cd2279cf1eff90c5428e7595c00849900142c
Samuel Eckermann <samuel.eckermann@maibornwolff.de>
2019-08-26 22:01:22 +0200

    trying things

--- src/main/java/io/vavr/hibernate/userstype/ListUserType.java
+++ src/main/java/io/vavr/hibernate/userstype/ListUserType.java
@@ -1,54 +1,55 @@
 package io.vavr.hibernate.userstype;
 
 import io.vavr.collection.List;
-import java.util.Iterator;
-import java.util.Map;
 import org.hibernate.HibernateException;
 import org.hibernate.collection.spi.PersistentCollection;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.usertype.UserCollectionType;
 
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * @author cbongiorno on 7/3/17.
  */
 public class ListUserType implements UserCollectionType {
 
-  @Override
-  public PersistentCollection instantiate(SharedSessionContractImplementor session,
-      CollectionPersister persister) throws HibernateException {
-    return null;
-  }
+    @Override
+    public PersistentCollection instantiate(SharedSessionContractImplementor session,
+                                            CollectionPersister persister) throws HibernateException {
+        return new PersistentList<>(session);
+    }
 
-  @Override
-  public PersistentCollection wrap(SharedSessionContractImplementor session, Object collection) {
-    return null;
-  }
+    @Override
+    public PersistentCollection wrap(SharedSessionContractImplementor session, Object collection) {
+        return new PersistentList<>(session, (List) collection);
+    }
 
-  @Override
-  public Iterator getElementsIterator(Object collection) {
-    return ((List)collection).iterator();
-  }
+    @Override
+    public Iterator getElementsIterator(Object collection) {
+        return ((PersistentList) collection).iterator();
+    }
 
-  @Override
-  public boolean contains(Object collection, Object entity) {
-    return ((List)collection).contains(entity);
-  }
+    @Override
+    public boolean contains(Object collection, Object entity) {
+        return ((PersistentList) collection).contains(entity);
+    }
 
-  @Override
-  public Object indexOf(Object collection, Object entity) {
-    return ((List)collection).indexOf(entity);
-  }
+    @Override
+    public Object indexOf(Object collection, Object entity) {
+        return ((PersistentList) collection).indexOf(entity);
+    }
 
-  @Override
-  public Object replaceElements(Object original, Object target, CollectionPersister persister,
-      Object owner, Map copyCache, SharedSessionContractImplementor session)
-      throws HibernateException {
-    return null;
-  }
+    @Override
+    public Object replaceElements(Object original, Object target, CollectionPersister persister,
+                                  Object owner, Map copyCache, SharedSessionContractImplementor session)
+            throws HibernateException {
+        return null;
+    }
 
-  @Override
-  public Object instantiate(int anticipatedSize) {
-    return List.empty();
-  }
+    @Override
+    public Object instantiate(int anticipatedSize) {
+        return List.empty();
+    }
 }
--- src/test/java/io/vavr/hibernate/ListUserTypeTest.java
+++ src/test/java/io/vavr/hibernate/ListUserTypeTest.java
@@ -1,12 +1,8 @@
 package io.vavr.hibernate;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import io.vavr.collection.LinearSeq;
 import io.vavr.collection.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import io.vavr.hibernate.userstype.PersistentList;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CollectionType;
@@ -16,66 +12,75 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.LinkedList;
 
 
 @RunWith(SpringRunner.class)
 public class ListUserTypeTest {
 
-  @Autowired
-  private PersonRepository people;
+    @Autowired
+    private PersonRepository people;
 
-  @SpringBootApplication
-  static class Configuration {
+    @SpringBootApplication
+    static class Configuration {
 
-  }
-
-
-  @Test
-  public void readsPersonIntoJavaslangOptionById() {
-
-    Person adrian = new Person("Adrian", "Bongiorno");
-    Person maya = new Person("Maya", "Bongiorno");
-    Person viola = new Person("Viola", "Bongiorno", List.of(adrian, maya));
-    Person me = new Person("Christian", "Bongiorno", List.of(adrian, maya));
-
-    Iterable<Person> people = this.people.save(List.of(adrian, maya, viola, me));
-
-    Person result = this.people.findOne(me.getId());
-
-    assertThat(result).isNotNull();
-    assertThat(result).isEqualTo(me);
-    assertThat(result.getChildren()).containsExactly(adrian, maya);
-  }
-
-  @Getter
-  @Entity
-  @NoArgsConstructor
-  public class Person {
-
-    private @GeneratedValue
-    @Id
-    Long id;
-    private String firstname;
-
-    private String lastname;
-
-    @ManyToMany
-    @CollectionType(type = "io.vavr.hibernate.userstype.ListUserType")
-    private List<Person> children;
-
-    public Person(String firstname, String lastname, List<Person> children) {
-      this.firstname = firstname;
-      this.lastname = lastname;
-      this.children = children;
     }
 
-    public Person(String firstname, String lastname) {
-      this(firstname, lastname, List.empty());
+
+    @Test
+    public void readsPersonIntoJavaslangOptionById() {
+
+        Person adrian = new Person("Adrian", "Bongiorno");
+        Person maya = new Person("Maya", "Bongiorno");
+        Person viola = new Person("Viola", "Bongiorno", List.of(adrian, maya));
+        Person me = new Person("Christian", "Bongiorno", List.of(adrian, maya));
+
+//    Iterable<Person> people = this.people.save(List.of(adrian, maya, viola, me).toJavaList());
+
+//    Person result = this.people.findOne(me.getId());
+//
+//    assertThat(result).isNotNull();
+//    assertThat(result).isEqualTo(me);
+//    assertThat(result.getChildren()).containsExactly(adrian, maya);
     }
-  }
 
-  public interface PersonRepository extends CrudRepository<Person, Long> {
+    @Getter
+    @Entity
+    @NoArgsConstructor
+    public class Person {
 
-  }
+        private @GeneratedValue
+        @Id
+        Long id;
+        private String firstname;
+
+        private String lastname;
+
+        @ManyToMany
+        @CollectionType(type = "io.vavr.hibernate.userstype.ListUserType2")
+        private List<Person> children;
+
+
+        public Person(String firstname, String lastname, List<Person> children) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.children = children;
+        }
+
+        public Person(String firstname, String lastname) {
+            this(firstname, lastname, List.empty());
+        }
+    }
+
+    @Component
+    public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    }
 }
